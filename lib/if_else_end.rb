48974c9cfd0f06df919987af59f0224f892740b2
lively-system-6174 <wkalada1@gmail.com>
2020-07-29 03:09:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,3 +1,9 @@
 # Write your solution here
 current_time = Time.now
-current_time.to_i % 
+even_or_odd_calc = (current_time.to_i % 2) == 0 
+#if there is no remainder then it is true (even) else odd 
+
+if even_or_odd_calc
+  puts "Even!"
+else 
+  puts "Odd!"
