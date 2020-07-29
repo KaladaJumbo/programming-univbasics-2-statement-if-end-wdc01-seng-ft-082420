289acb95bb61bd32ce13a90883d430738f9bc9a5
lively-system-6174 <wkalada1@gmail.com>
2020-07-29 02:38:17 +0000

    Automatically backed up by Learn

--- learning-ifs.rb
+++ learning-ifs.rb
@@ -2,7 +2,7 @@ chance_of_rain = 0.2
 puts "lets go outside"
 if chance_of_rain <= 0.25 
   puts "pack a sun shelter"
-elseif (chance_of_rain > 0.25 && chance_of_rain < 0.75) 
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75) 
   puts "Pack an umbrella"
 else 
   puts "stay home and read hegal"
