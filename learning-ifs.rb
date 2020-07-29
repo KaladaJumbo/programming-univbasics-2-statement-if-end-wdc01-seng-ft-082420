e92043b864b44f28e0735772c10a71b43d61e2da
lively-system-6174 <wkalada1@gmail.com>
2020-07-29 02:59:32 +0000

    Automatically backed up by Learn

--- learning-ifs.rb
+++ learning-ifs.rb
@@ -2,8 +2,8 @@ this_year = "no"
 while this_year != "exit"
   puts "hey what year is it? \n"
   this_year = gets.chomp
-  if (this_year == "2020")
-  puts " this is the worst year ever"
+  
+  puts " this is the worst year ever"if (this_year == "2020")
   else 
     puts "oh ... happy new year"
   end
